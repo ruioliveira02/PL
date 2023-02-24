00bac8d4caa335acca2d5976f1d1df52667e6d44
Rui Oliveira <rpoliveira02@gmail.com>
2023-02-24 11:46:01 +0000

    TPC2

---
+++ TPC2/tpc2.py
@@ -0,0 +1,21 @@
+def main():
+    data = input()
+    s = ""
+    n = len(data)
+    on = True
+    res = 0
+    for i in range(0,n):
+        if i >= 2 and data[i - 2:i + 1].lower() == "off":
+            on = False
+        elif i >= 1 and data[i - 1:i + 1].lower() == "on":
+            on = True
+        elif data[i] == '=':
+            print(res)
+        elif data[i].isnumeric() and on:
+            s += data[i]
+        elif s != "" and on:
+            res += int(s)
+            s = ""
+
+if __name__ != "main":
+    main()
